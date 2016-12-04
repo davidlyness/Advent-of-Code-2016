8c94b185b9d9f1ccb806e0e801da52b33b49e97d
David Lyness <david@davidlyness.com>
2016-12-04 13:44:36 +0000

    Add solution for Day 1

---
+++ 1/1b.py
@@ -0,0 +1,60 @@
+# coding=utf-8
+"""Advent of Code 2016, Day 1, Part 2"""
+
+
+def turn(direction, action):
+    """
+    Calculates the new direction to face.
+    :param direction: the starting direction
+    :param action: whether to turn left (L) or right (R)
+    :return: the new direction after turning
+    """
+    directions = ["north", "east", "south", "west"]
+    if action == "L":
+        return directions[(directions.index(direction) - 1) % 4]
+    else:
+        return directions[(directions.index(direction) + 1) % 4]
+
+
+def move(position, direction):
+    """
+    Calculates the new position of the sleigh.
+    :param position: the starting position
+    :param direction: the direction in which to move
+    :return: the new position after movement
+    """
+    if direction == "north":
+        return position[0], position[1] + 1
+    elif direction == "east":
+        return position[0] + 1, position[1]
+    elif direction == "south":
+        return position[0], position[1] - 1
+    elif direction == "west":
+        return position[0] - 1, position[1]
+    return position
+
+
+def find_first_crossover(instructions):
+    """
+    Calculates the first position visited twice.
+    :param instructions: direction and movement instructions
+    :return: the number of blocks between the current position and starting position
+    """
+    current_direction = "north"
+    current_position = (0, 0)
+    visited_positions = [current_position]
+    for instruction in instructions:
+        current_direction = turn(current_direction, action=instruction[:1])
+        current_distance = int(instruction[1:])
+        for _ in range(current_distance):
+            current_position = move(current_position, current_direction)
+            if current_position in visited_positions:
+                return abs(current_position[0]) + abs(current_position[1])
+            else:
+                visited_positions.append(current_position)
+
+
+with open("input.txt") as f:
+    data = f.read().replace("\n", "").split(", ")
+
+print(find_first_crossover(data))
