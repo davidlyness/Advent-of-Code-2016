ea0b5fd91ee4a06dd6ac04bb6c8cffa6d7b8729b
David Lyness <david@davidlyness.com>
2016-12-07 22:05:27 +0000

    Add solution for Day 6

---
+++ 6/6b.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+"""Advent of Code 2016, Day 6, Part 2"""
+
+with open("input.txt") as f:
+    data = f.read().rstrip("\n").split()
+
+message = ""
+message_length = len(data[0])
+
+for i in range(message_length):
+    frequencies = {}
+    for transmission in data:
+        if transmission[i] not in frequencies:
+            frequencies[transmission[i]] = 0
+        frequencies[transmission[i]] += 1
+    message += min(frequencies.keys(), key=(lambda k: frequencies[k]))
+
+print(message)
