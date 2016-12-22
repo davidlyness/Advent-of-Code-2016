2cbbf648ffec0b2fb53573a2c8e9bc5a65ea2413
David Lyness <david@davidlyness.com>
2016-12-22 21:52:30 +0000

    Add solution for Day 17

---
+++ 17/17a.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+"""Advent of Code 2016, Day 17, Part 1"""
+
+import hashlib
+
+
+def get_next_positions(position, path):
+    """
+    Get all valid next positions by moving up, down, left or right from the current position.
+    :param position: current position
+    :param path: the prior path already taken
+    :return: list of tuples of (valid next position, associated direction)
+    """
+    open_whitelist = ["b", "c", "d", "e", "f"]
+    candidate_states = []
+    up, down, left, right = hashlib.md5(path.encode()).hexdigest()[:4]
+    if up in open_whitelist and position[0] > 0:
+        candidate_states.append(((position[0] - 1, position[1]), "U"))
+    if down in open_whitelist and position[0] < 3:
+        candidate_states.append(((position[0] + 1, position[1]), "D"))
+    if left in open_whitelist and position[1] > 0:
+        candidate_states.append(((position[0], position[1] - 1), "L"))
+    if right in open_whitelist and position[1] < 3:
+        candidate_states.append(((position[0], position[1] + 1), "R"))
+    return candidate_states
+
+with open("input.txt") as f:
+    puzzle_input = f.read().rstrip("\n")
+
+start_position = (0, 0)
+goal_position = (3, 3)
+shortest_path = None
+config_queue = [(start_position, puzzle_input)]
+
+while not shortest_path:
+    current_position, current_path = config_queue.pop(0)
+    if current_position == goal_position:
+        shortest_path = current_path[len(puzzle_input):]
+    else:
+        for next_position, direction in get_next_positions(current_position, current_path):
+            config_queue.append((next_position, current_path + direction))
+
+print(shortest_path)
