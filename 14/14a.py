08df011cf5a5e6dd164bfa44ec44abe09ab71be7
David Lyness <david@davidlyness.com>
2016-12-22 19:23:45 +0000

    Add solution for Day 14

---
+++ 14/14a.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+"""Advent of Code 2016, Day 14, Part 1"""
+
+import hashlib
+
+
+def get_hash(string):
+    """
+    Calculate the MD5 hash of a given string.
+    :param string: the given string
+    :return: hexadecimal hash of the string
+    """
+    return hashlib.md5(string.encode()).hexdigest()
+
+
+def get_repeating_sequence(string):
+    """
+    Determine the first thrice-repeating character within a string.
+    :param string: the given string
+    :return: the repeating character if it exists; None otherwise
+    """
+    for position in range(len(string) - 2):
+        sequence = string[position:position+3]
+        if sequence == string[position] * 3:
+            return sequence[0]
+    return None
+
+with open("input.txt") as f:
+    salt = f.read().rstrip("\n")
+
+hashes = []
+key_indexes = []
+
+for i in range(1000):
+    hashes.append(get_hash(salt + str(i)))
+
+index = 0
+while len(key_indexes) < 64:
+    hashes.append(get_hash(salt + str(index+1000)))
+    candidate_key = hashes[index]
+    candidate_character = get_repeating_sequence(candidate_key)
+    if candidate_character:
+        extended_sequence = candidate_character * 5
+        if any(extended_sequence in hashed_string for hashed_string in hashes[index+1:index+1001]):
+            key_indexes.append(index)
+    index += 1
+
+print(key_indexes[-1])
